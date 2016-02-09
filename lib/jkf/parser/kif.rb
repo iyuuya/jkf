2b279adb731f554d82ba4448b8d60c1e0d5dc6a0
iyuuya <i.yuuya@gmail.com>
2016-02-09 18:49:44 +0900

    add skipline rule

--- lib/jkf/parser/kif.rb
+++ lib/jkf/parser/kif.rb
@@ -1,6 +1,16 @@
 module Jkf
   module Parser
     class Kif < Parslet::Parser
+      root :kifu
+
+      rule(:kifu) { skipline.repeat.maybe }
+
+      # whitespace / nl / nonl
+      rule(:nl) { newline.repeat(1) >> skipline.repeat.maybe }
+      rule(:nonl) { match('[^\n]') }
+      rule(:whitespace) { match('\s') }# str(' ') | str("\t") }
+      rule(:newline) { whitespace.repeat.maybe >> (match('\n') | match('\r\n?')) }
+      rule(:skipline) { str('#') >> nonl.repeat.maybe >> newline }
     end
   end
 end

fdcc84c0ad2a6e84534f5950f3c6caae997788a4
iyuuya <i.yuuya@gmail.com>
2016-02-09 18:52:26 +0900

    add header rule

--- lib/jkf/parser/kif.rb
+++ lib/jkf/parser/kif.rb
@@ -3,12 +3,20 @@ module Jkf
     class Kif < Parslet::Parser
       root :kifu
 
-      rule(:kifu) { skipline.repeat.maybe }
+      rule(:kifu) { skipline.repeat.maybe >> header.repeat.maybe.as(:headers) }
+
+      # Header
+      rule(:header) {
+        str("盤面回転").as(:kaiten) >> nl |
+        turn.as(:te) >> str("手番") >> nl |
+        match('[^：\r\n]').repeat.as(:key) >> str("：") >> nonl.repeat.maybe.as(:value) >> nl
+      }
+      rule(:turn) { match('[先後上下]') }
 
       # whitespace / nl / nonl
       rule(:nl) { newline.repeat(1) >> skipline.repeat.maybe }
       rule(:nonl) { match('[^\n]') }
-      rule(:whitespace) { match('\s') }# str(' ') | str("\t") }
+      rule(:whitespace) { str(' ') | str("\t") }
       rule(:newline) { whitespace.repeat.maybe >> (match('\n') | match('\r\n?')) }
       rule(:skipline) { str('#') >> nonl.repeat.maybe >> newline }
     end

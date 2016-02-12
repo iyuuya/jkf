add2bb435380bb2d87f4107364eb8ae6d3359f22
iyuuya <i.yuuya@gmail.com>
2016-02-13 01:45:32 +0900

    add parse file examples

--- spec/jkf/parser/ki2_spec.rb
+++ spec/jkf/parser/ki2_spec.rb
@@ -1,4 +1,24 @@
 require 'spec_helper'
+require 'kconv'
 
 describe Jkf::Parser::Ki2 do
+  let(:ki2_parser) { Jkf::Parser::Ki2.new }
+  subject { ki2_parser.parse(str) }
+
+  shared_examples(:parse_file) do |filename|
+    let(:str) do
+      if File.extname(filename) == '.ki2'
+        File.read(filename, encoding: 'Shift_JIS').toutf8
+      else
+        File.read(filename).toutf8
+      end
+    end
+    it "should be parse #{File.basename(filename)}" do
+      is_expected.not_to be_nil
+    end
+  end
+
+  fixtures(:ki2).each do |fixture|
+    it_behaves_like :parse_file, fixture
+  end
 end

eaebe8062b2e5ccb2147002dcb46c4257a808f78
iyuuya <i.yuuya@gmail.com>
2016-02-15 10:20:56 +0900

    preparete convert

--- lib/jkf/converter/kif.rb
+++ lib/jkf/converter/kif.rb
@@ -1,6 +1,29 @@
 module Jkf::Converter
   class Kif
     def convert(jkf)
+      hash = JSON.parse(jkf)
+
+      result = ''
+      result += convert_header(hash['header']) if hash['header']
+      result += convert_initial(hash['initial']) if hash['initial']
+      result += convert_moves(hash['moves']) if hash['moves']
+
+      result
+    end
+
+    protected
+
+    # TODO: implements
+    def convert_header(header)
+      result = ''
+    end
+
+    def convert_initial(initial)
+      result = ''
+    end
+
+    def convert_moves(moves)
+      result = ''
     end
   end
 end

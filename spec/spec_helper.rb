1e374fe7d38adea37a336886cef6e9f35ddfac2d
iyuuya <i.yuuya@gmail.com>
2016-02-21 17:45:21 +0900

    fix spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require 'kconv'
+require 'pry'
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'jkf'
@@ -12,6 +13,10 @@ end
 module IncludeHelper
   def pos(x, y)
     { 'x' => x, 'y' => y }
+  end
+
+  def fixtures(type)
+    Dir[File.expand_path("../fixtures/#{type}/**", __FILE__)]
   end
 end
 

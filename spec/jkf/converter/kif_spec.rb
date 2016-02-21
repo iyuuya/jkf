d0db9c2e510cd2fa5d539f9e9b23804b1bc271ff
iyuuya <i.yuuya@gmail.com>
2016-02-21 22:41:57 +0900

    add kif converter examples

--- spec/jkf/converter/kif_spec.rb
+++ spec/jkf/converter/kif_spec.rb
@@ -1,4 +1,27 @@
 require 'spec_helper'
 
 describe Jkf::Converter::Kif do
+  let(:kif_converter) { Jkf::Converter::Kif.new }
+  let(:kif_parser) { Jkf::Parser::Kif.new }
+
+  subject { kif_parser.parse(kif_converter.convert(jkf)) }
+
+  shared_examples(:parse_file) do |filename|
+    let(:str) do
+      if File.extname(filename) == '.kif'
+        File.read(filename, encoding: 'Shift_JIS').toutf8
+      else
+        File.read(filename).toutf8
+      end
+    end
+    let(:jkf) { kif_parser.parse(str).to_json }
+
+    it "should be parse #{File.basename(filename)}" do
+      is_expected.to eq JSON.parse(jkf)
+    end
+  end
+
+  fixtures(:kif).each do |fixture|
+    it_behaves_like :parse_file, fixture
+  end
 end

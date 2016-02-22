a8447567349bda76315cbd42c27e16bbf2fa40d5
iyuuya <i.yuuya@gmail.com>
2016-02-22 14:23:09 +0900

    add csa examples

--- spec/jkf/converter/csa_spec.rb
+++ spec/jkf/converter/csa_spec.rb
@@ -1,4 +1,27 @@
 require 'spec_helper'
 
 describe Jkf::Converter::Csa do
+  let(:csa_converter) { Jkf::Converter::Csa.new }
+  let(:csa_parser) { Jkf::Parser::Csa.new }
+
+  subject { csa_parser.parse(csa_converter.convert(jkf)) }
+
+  shared_examples(:parse_file) do |filename|
+    let(:str) do
+      if File.extname(filename) == '.csa'
+        File.read(filename, encoding: 'Shift_JIS').toutf8
+      else
+        File.read(filename).toutf8
+      end
+    end
+    let(:jkf) { csa_parser.parse(str).to_json }
+
+    it "should be parse #{File.basename(filename)}" do
+      is_expected.to eq JSON.parse(jkf)
+    end
+  end
+
+  fixtures(:csa).each do |fixture|
+    it_behaves_like :parse_file, fixture
+  end
 end

af963f242464633239a39a93f6f73c00796a84ef
iyuuya <i.yuuya@gmail.com>
2016-02-25 11:33:14 +0900

    jkf/converter/base.rb

--- lib/jkf/converter/base.rb
+++ lib/jkf/converter/base.rb
@@ -1,7 +1,10 @@
 module Jkf::Converter
+  # Base of Converter
   class Base
+    # start convert
+    # 
     # @param [String, Hash] jkf
-    # @return [String]
+    # @return [String] kif or ki2 or csa text
     def convert(jkf)
       jkf = jkf.is_a?(Hash) ? jkf : JSON.parse(jkf)
       convert_root(jkf)
